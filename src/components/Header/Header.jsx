9800c7d83064029a0caa9d4e2c238c4b381ebaea
Bastien Winant <bastien.winant@gmail.com>
2025-02-17 13:56:47 +0100

    add window width observer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import './index.css'
 import closeIcon from './img/e-remove.svg'
 import menuBurgerIcon from './img/menu-8.svg'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
   const [navExpanded, setNavExpanded] = useState(false)
+  const [wideWindow, setWideWindow] = useState(window.innerWidth >= 768)
+    
+  useEffect(() => {
+    function watchWindowWidth () {
+      setNavExpanded(false)
+      setWideWindow(window.innerWidth >= 768)
+    }
+    window.addEventListener("resize", watchWindowWidth)
+    return () => window.removeEventListener("resize", watchWindowWidth)
+  }, [])
 
   function toggleNav() {
     setNavExpanded(prevVal => !prevVal)
@@ -15,7 +25,7 @@ export default function Header() {
       <button type="button" className="nav--expand-btn" onClick={toggleNav}>
         <img src={menuBurgerIcon} alt="Hamburger menu icon." className="nav--btn-img" />
       </button>
-      {navExpanded && <nav className="nav">
+      {(navExpanded || wideWindow) && <nav className="nav">
         <button type="button" className='nav--collapse-btn' onClick={toggleNav}>
           <img src={closeIcon} alt="Close icon." className="nav--btn-img" />
         </button>
