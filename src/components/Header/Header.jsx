c87148363230bfe6223dc0c5e95c14e940473114
Bastien Winant <bastien.winant@gmail.com>
2025-03-23 23:44:25 +0100

    collapsible header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,39 @@
+import { useState, useEffect } from 'react'
 import './index.css'
+import hamburgerIcon from './img/12-menu-8.svg'
+import closeIcon from './img/12-e-remove.svg'
 
 export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [expanded, setExpanded] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setExpanded(false)
+      setWindowWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  function toggleNav() {
+    setExpanded(prevVal => !prevVal)
+  }
+
   return (
     <header className="header">
       <div className="header--container">
-        <nav className="nav">
+        <button type="button" className="header--nav-toggle-btn" onClick={toggleNav}>
+          <img src={expanded ? closeIcon : hamburgerIcon} alt="Nav hamburger icon." />
+        </button>
+        {(expanded || windowWidth >= 768) && <nav className="nav">
           <ul className="nav-links">
             <li><a href="#" className="nav-link">work</a></li>
             <li><a href="#" className="nav-link">about</a></li>
             <li><a href="#" className="nav-link">contact</a></li>
           </ul>
-        </nav>
+        </nav>}
       </div>
     </header>
   )
