e722f51be1bfbacccda724df7d1cd55d47bd86b4
Bastien Winant <bastien.winant@gmail.com>
2025-03-23 23:53:07 +0100

    project component skeleton

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,12 @@
+export default function Project() {
+  return (
+    <article className="project">
+      <header className="project--header">
+        <h3 className="project--name">project name</h3>
+      </header>
+      <div className="project--body">
+        
+      </div>
+    </article>
+  )
+}
