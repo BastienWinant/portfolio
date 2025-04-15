ec4a97e2f33339c2c03c33ec7c3546ba2111147e
Bastien Winant <bastien.winant@gmail.com>
2025-04-15 10:31:41 +0200

    card font size and spacing

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Image, Box, Wrap, Badge, LinkBox, LinkOverlay } from "@chakra-ui/react"
+import { Button, Card, Image, Wrap, Badge, Box, LinkBox, LinkOverlay } from "@chakra-ui/react";
 import { nanoid } from "nanoid";
 
 export default function Project(props) {
@@ -7,28 +7,27 @@ export default function Project(props) {
   return (
     <Card.Root flexDir={{base: "column", md: "row"}} overflow="hidden">
       <Image
+        objectFit="cover"
         src={imgUrl}
-        alt="Green double couch with wooden legs"
+        alt="Project screenshot"
         w={{md: "1/2"}}
         aspectRatio={2 / 1}
       />
       <Box>
-        <Card.Body gap="2">
-          <Card.Title textTransform="capitalize">{name}</Card.Title>
-          <Card.Description>
-            {description}
-          </Card.Description>
+        <Card.Body gap="3">
+          <Card.Title textTransform="capitalize" fontSize={{base: "xl", md: "2xl"}}>{name}</Card.Title>
+          <Card.Description fontSize={{base: "md", md: "lg"}}>{description}</Card.Description>
           <Wrap mt="4">
-            {techStack.map(tech => <Badge key={nanoid()}>{tech}</Badge>)}
+            {techStack.map(tech => <Badge>{tech}</Badge>)}
           </Wrap>
         </Card.Body>
         <Card.Footer gap="2">
           <LinkBox>
-            <Badge variant="solid" size="lg" px="4" py="2">Live site</Badge>
+            <Badge size="lg" px="4" py="2.5" variant="solid">Buy now</Badge>
             <LinkOverlay href="https://github.com/BastienWinant" target="_blank" />
           </LinkBox>
           <LinkBox>
-            <Badge variant="ghost" size="lg" px="4" py="2">Code</Badge>
+            <Badge size="lg" px="4" py="2.5" variant="plain">Add to cart</Badge>
             <LinkOverlay href="https://github.com/BastienWinant" target="_blank" />
           </LinkBox>
         </Card.Footer>
