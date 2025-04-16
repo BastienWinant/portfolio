dcae24dfda9490b920bd0ef034229ab5879992b7
Bastien Winant <bastien.winant@gmail.com>
2025-04-16 12:35:51 +0200

    footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { Box, Container } from "@chakra-ui/react"
+
+export default function Footer() {
+  return (
+    <Box as="footer">
+      <Container w={{md: "11/12"}}>
+      </Container>
+    </Box>
+  )
+}
