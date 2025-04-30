226c38197649499ab809c50a57f9835c0a9fa368
Bastien Winant <bastien.winant@gmail.com>
2025-04-30 12:00:57 +0200

    refactor section component

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -2,16 +2,14 @@ import { Box, Container, Stack } from '@chakra-ui/react'
 
 export default function Section({children, ...props}) {
 	return (
-		<Box
-			as={"section"}
+		<Container
+			as="section"
 			py={{base: 16, md: 20, lg: 24}}
 			{...props}
 		>
-			<Container>
-				<Stack gap={{base: 12, md: 16 }}>
-					{children}
-				</Stack>
-			</Container>
-		</Box>
+			<Stack>
+				{children}
+			</Stack>
+		</Container>
 	)
 }
