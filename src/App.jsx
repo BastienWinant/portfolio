a1425913b83d6ec00c97d77fb656c5a62b7eac93
Bastien Winant <bastien.winant@gmail.com>
2025-05-06 23:44:05 +0200

    create project detail page and route

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
+          <Route path="projects/:name" element={} />
         </Route>
       </Routes>
     </Router>
