e90a69725f3c8b6df5a3ce7d35f33c3954603ee9
BastienWinant <bastien.winant@gmail.com>
2025-07-02 23:07:13 +0200

    header avatar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,17 @@
-import { Box } from "@chakra-ui/react"
+import { Box, HStack, Avatar, Stack, Text } from "@chakra-ui/react"
 
 export default function Header() {
 	return (
-			<Box borderWidth="1px" h="5">
-				i am testing
+			<Box borderWidth="4px">
+				<HStack gap="4">
+					<Avatar.Root size="xl">
+						<Avatar.Fallback name="Bastien Winant" />
+						<Avatar.Image src="https://images.unsplash.com/photo-1745177717290-9ce463cdc5e1" />
+					</Avatar.Root>
+					<Stack gap="0">
+						<Text fontWeight="medium">Bastien Winant</Text>
+					</Stack>
+				</HStack>
 			</Box>
 	)
 }
