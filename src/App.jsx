e31abbcc0e0de10049c3e0b13537e4a18fe1c6a2
BastienWinant <bastien.winant@gmail.com>
2025-07-02 22:46:06 +0200

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from "@/components/header/header.jsx";
 
 function App() {
   return (
     <>
-      this is the app
+      <Header />
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+
+export default function Header() {
+	return (
+			<Box borderWidth="1px" h="5">
+				i am testing
+			</Box>
+	)
+}
