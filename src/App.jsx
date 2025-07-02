282b573fd6d44f564d4ff6d7e54ea72840125f61
BastienWinant <bastien.winant@gmail.com>
2025-07-02 22:39:22 +0200

    new color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router"
-import Layout from "@/components/layout/Layout.jsx"
-import Main from "@/pages/Main.jsx"
-import ProjectDetail from "@/pages/ProjectDetail.jsx"
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="projects/:name" element={<ProjectDetail />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      this is the app
+    </>
   )
 }
 
--- src/pages/ProjectDetail.jsx
+++
@@ -1,6 +0,0 @@
-import {Heading} from '@chakra-ui/react'
-export default function ProjectDetail() {
-	return (
-		<Heading size="6xl">this is the projects page</Heading>
-	)
-}
