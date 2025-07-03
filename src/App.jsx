0478e4daa4b5a38eb8acb6c5f9cc1b26dbbbf78d
BastienWinant <bastien.winant@gmail.com>
2025-07-03 12:51:01 +0200

    set up react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router";
 import Header from "@/components/header/header.jsx";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <Routes>
+        <Route index  element={<h1>this is the home page</h1>}/>
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
-import { Box, HStack, Avatar, Stack, Text } from "@chakra-ui/react"
+import { Stack, HStack, Avatar, Text, Link as ChakraLink } from "@chakra-ui/react"
+import { NavLink } from "react-router"
 import avatarImg from '@/assets/img/profile.png'
 
 export default function Header() {
 	return (
-			<Box borderWidth="4px">
+			<Stack as="header" borderWidth="4px">
 				<HStack gap="4" border="3px solid red">
 					<Avatar.Root>
 						<Avatar.Fallback name="Bastien Winant" />
@@ -13,6 +14,11 @@ export default function Header() {
 						<Text fontWeight="medium">Bastien Winant</Text>
 					</Stack>
 				</HStack>
-			</Box>
+				<Stack as="nav">
+					<Link asChild>
+						<Li href="/">Home</Li>
+					</Link>
+				</Stack>
+			</Stack>
 	)
 }
