fff599f105cf64ef1e53155988ead056cf2a9da1
BastienWinant <bastien.winant@gmail.com>
2025-07-04 21:29:51 +0200

    project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Layout from "@/components/layout/Layout.jsx";
 import Home from "@/pages/Home.jsx";
+import ProjectDetail from "@/pages/ProjectDetail.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="projects/:id" element={<ProjectDetail />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,9 @@
+import { useParams} from "react-router";
+
+export default function ProjectDetail() {
+	const params = useParams();
+
+	return (
+			<>project page</>
+	)
+}
