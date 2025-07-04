ebb81cc8142a90fa6931028f0bb50635cd5aeac7
BastienWinant <bastien.winant@gmail.com>
2025-07-04 17:45:11 +0200

    refactor vite config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route} from "react-router";
-import Layout from "@/components/layout/Layout.jsx"
-import Home  from "@/pages/Home.jsx"
-import ProjectDetail from "@/pages/ProjectDetail.jsx";
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path="projects/:id" element={<ProjectDetail />} />
-          <Route path="test"  element={<h1>this is the test page</h1>}/>
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsconfigPaths from "vite-jsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [react(), jsconfigPaths()],
 })
