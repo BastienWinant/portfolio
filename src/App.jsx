37f0687b1167e08763e37ab27f8b2c186a949f6b
BastienWinant <bastien.winant@gmail.com>
2025-07-04 08:47:52 +0200

    create layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from "react-router";
+import Layout from "@/components/layout/Layout.jsx"
 import Home  from "@/pages/Home.jsx"
 import ProjectDetail from "@/pages/ProjectDetail.jsx";
-// import Header from "@/components/header/header.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/">
-          <Route index  element={<Home />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
           <Route path="projects/:id" element={<ProjectDetail />} />
           <Route path="test"  element={<h1>this is the test page</h1>}/>
         </Route>
