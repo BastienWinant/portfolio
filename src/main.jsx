4fb1ab7545e125078796dca3dadbbb5a421c2d55
BastienWinant <bastien.winant@gmail.com>
2025-07-05 15:31:42 +0200

    wrap app in nav context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+import { Provider as ChakraProvider } from "@/components/ui/provider";
+import { NavProvider } from "@/providers/navContext/NavProvider.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <NavProvider>
+        <App />
+      </NavProvider>
+    </ChakraProvider>
   </StrictMode>,
 )
