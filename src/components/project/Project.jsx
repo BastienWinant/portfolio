f1e32fb9bae09ddb5a34ccdabc2abd90d97d9b51
BastienWinant <bastien.winant@gmail.com>
2025-07-09 19:54:05 +0200

    card size and style

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,8 +1,17 @@
 import { Button, Card, Image, Wrap, Flex, Badge } from "@chakra-ui/react"
 
 export default function Projects() {
+	const title="Quiz App"
+	const description = "Quizzical is a simple quiz app that lets you select alternatives and then gives you a score. It's built with functional React components."
+	const techStack = ['HTML', 'CSS', 'JavaScript', 'React']
 	return (
-			<Card.Root flexDirection={{base: "column", md: "row"}} overflow="hidden" w="100%">
+			<Card.Root
+					flexDirection={{base: "column", md: "row"}}
+					overflow="hidden"
+					w="100%"
+					variant="elevated"
+					size="lg"
+			>
 				<Image
 						src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
 						alt="Green double couch with wooden legs"
@@ -11,14 +20,10 @@ export default function Projects() {
 				/>
 				<Flex direction="column" justify="center" flexGrow="1">
 					<Card.Body gap="2" flexGrow="0">
-						<Card.Title>Living room Sofa</Card.Title>
-						<Card.Description>
-							This sofa is perfect for modern tropical spaces, baroque inspired
-							spaces.
-						</Card.Description>
+						<Card.Title>{title}</Card.Title>
+						<Card.Description>{description}</Card.Description>
 						<Wrap mt="4">
-							<Badge>Hot</Badge>
-							<Badge>Caffeine</Badge>
+							{techStack.map(tech => <Badge key={tech} size="md">{tech}</Badge>)}
 						</Wrap>
 					</Card.Body>
 					<Card.Footer>
