0c63754e80f8b106727d3e20d4dab2ee10f6f7a4
BastienWinant <bastien.winant@gmail.com>
2025-07-09 19:20:56 +0200

    wrap card text in a box

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,27 +1,28 @@
-import { Button, Card, Image, Wrap, Badge } from "@chakra-ui/react"
+import { Button, Card, Image, Wrap, Box, Badge } from "@chakra-ui/react"
 
-export default function Project() {
+export default function Projects() {
 	return (
-			<Card.Root maxW="sm" overflow="hidden" border="2px solid orange;">
+			<Card.Root maxW="sm" overflow="hidden">
 				<Image
 						src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
 						alt="Green double couch with wooden legs"
 				/>
-				<Card.Body gap="2" borderWidth="thin">
-					<Card.Title>Personal Dashboard</Card.Title>
-					<Card.Description>
-						A Chrome extension to help you focus and stay up-to-date.
-					</Card.Description>
-					<Wrap mt="4">
-						<Badge>HTML</Badge>
-						<Badge>CSS</Badge>
-						<Badge>JavaScript</Badge>
-						<Badge>React</Badge>
-					</Wrap>
-				</Card.Body>
-				<Card.Footer>
-					<Button>Learn More</Button>
-				</Card.Footer>
+				<Box>
+					<Card.Body gap="2">
+						<Card.Title>Living room Sofa</Card.Title>
+						<Card.Description>
+							This sofa is perfect for modern tropical spaces, baroque inspired
+							spaces.
+						</Card.Description>
+						<Wrap mt="4">
+							<Badge>Hot</Badge>
+							<Badge>Caffeine</Badge>
+						</Wrap>
+					</Card.Body>
+					<Card.Footer>
+						<Button>Buy Latte</Button>
+					</Card.Footer>
+				</Box>
 			</Card.Root>
 	)
 }
