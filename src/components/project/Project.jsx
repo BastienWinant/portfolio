1cc80268f6d8cd6c0a8ec535bdf22a79ea1efa80
BastienWinant <bastien.winant@gmail.com>
2025-07-09 18:53:45 +0200

    card badges

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,8 +1,8 @@
-import { Button, Card, Image, Text } from "@chakra-ui/react"
+import { Button, Card, Image, HStack, Badge } from "@chakra-ui/react"
 
 export default function Project() {
 	return (
-			<Card.Root maxW="sm" overflow="hidden">
+			<Card.Root maxW="sm" overflow="hidden" border="2px solid orange;">
 				<Image
 						src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
 						alt="Green double couch with wooden legs"
@@ -13,13 +13,13 @@ export default function Project() {
 						This sofa is perfect for modern tropical spaces, baroque inspired
 						spaces.
 					</Card.Description>
-					<Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
-						$450
-					</Text>
+					<HStack mt="4">
+						<Badge>Hot</Badge>
+						<Badge>Caffeine</Badge>
+					</HStack>
 				</Card.Body>
-				<Card.Footer gap="2">
-					<Button variant="solid">Buy now</Button>
-					<Button variant="ghost">Add to cart</Button>
+				<Card.Footer>
+					<Button>Buy Latte</Button>
 				</Card.Footer>
 			</Card.Root>
 	)
