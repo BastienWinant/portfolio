d601c323dd8df8e740cbd7fde0abc538aa972fc5
BastienWinant <bastien.winant@gmail.com>
2025-07-21 12:21:05 +0200

    create project card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header.jsx";
 import Landing from "./components/landing/Landing.jsx";
 import Section from "./components/section/index.jsx";
+import ProjectCard from "./components/projectCard/ProjectCard.jsx";
 import {useNav} from "./contexts/navContext/NavContext.js";
 import './App.css'
 
@@ -14,11 +15,38 @@ function App() {
         <Landing />
         <Section ref={workRef} className="work">
           <Section.Header>my work</Section.Header>
-          <Section.Body>this is the work section</Section.Body>
+          <Section.Body>
+            <div className="work__projects">
+              <ProjectCard
+                  imgUrl="https://images.unsplash.com/photo-1752350434950-50e8df9c268e"
+                  title="Quiz App"
+                  description="Quizzical is a simple quiz app that lets you select alternatives and then gives you a score. It's built with functional React components."
+                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  demoLink="#"
+                  githubLink="#"
+              />
+              <ProjectCard
+                  imgUrl="https://images.unsplash.com/photo-1752654977019-8578b2905411"
+                  title="Personal Dashboard"
+                  description="A Chrome extension to help you focus and stay up-to-date."
+                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  demoLink="#"
+                  githubLink="#"
+              />
+              <ProjectCard
+                  imgUrl="https://images.unsplash.com/photo-1744878150591-6ebf3a050d4f"
+                  title="Password Generator"
+                  description="An app that generates random passwords based on a few user inputs, e.g. length, types of characters, etc. Built with HTML, CSS, and vanilla JavaScript."
+                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  demoLink="#"
+                  githubLink="#"
+              />
+            </div>
+          </Section.Body>
         </Section>
         <Section ref={aboutRef}>
           <Section.Header>about me</Section.Header>
-          <Section.Body>this is the about section</Section.Body>
+          <Section.Body>about section</Section.Body>
         </Section>
         <Section ref={contactRef}>
           <Section.Header>get in touch</Section.Header>
---
+++ src/components/projectCard/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import {nanoid} from "nanoid";
+import "./ProjectCard.css"
+import { FaArrowRightLong, FaGithub } from "react-icons/fa6";
+
+export default function ProjectCard({imgUrl, title, description, techStack, githubLink, demoLink}) {
+	return (
+		<figure className="project">
+			<img className="project__img" src={imgUrl} alt="Project screenshot." />
+			<figcaption className="project__caption">
+				<header className="project__header">
+					<h3 className="project__title">{title}</h3>
+				</header>
+				<section>
+					<p className="project__description">{description}</p>
+					<ul className="project__techstack">
+						{techStack.map(tech => <li key={nanoid()} className="project__tech">{tech}</li>)}
+					</ul>
+				</section>
+				<footer className="project__footer">
+					<a className="project__github_link" href={githubLink} target="_blank"><FaGithub /></a>
+				</footer>
+			</figcaption>
+		</figure>
+	)
+}
