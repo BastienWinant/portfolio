dd30adc8f58cd5dc0eb03590f0c2627b0dc708fb
BastienWinant <bastien.winant@gmail.com>
2025-07-22 11:38:33 +0200

    project descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,24 @@ function App() {
               <ProjectCard
                   imgUrl="https://images.unsplash.com/photo-1752834370400-da734c87f565"
                   title="Quiz App"
-                  description="Simple quiz app that lets you select alternatives and then gives you a score."
-                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  description="Fictional online platform for renting personal parking spots near major venues."
+                  techStack={['TypeScript', 'React', 'Firebase Realtime Database', 'TicketMaster API']}
                   demoLink="#"
                   githubLink="#"
               />
               <ProjectCard
-                  imgUrl="https://images.unsplash.com/photo-1752835178514-f31f93a3b540"
-                  title="Personal Dashboard"
-                  description="A Chrome extension to help you focus and stay up-to-date."
-                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  imgUrl="https://images.unsplash.com/photo-1617380613434-7495e9b45dfb"
+                  title="Himalayan DB API"
+                  description="A RESTful API that provides access to the Himalayan DB data."
+                  techStack={['Django', 'PostgreSQL', 'AWS API Gateway']}
                   demoLink="#"
                   githubLink="#"
               />
               <ProjectCard
                   imgUrl="https://images.unsplash.com/photo-1752805252779-000e9d493b1f"
-                  title="Password Generator"
-                  description="An app that generates random passwords based on a few user inputs."
-                  techStack={['HTML', 'CSS', 'JavaScript', 'React']}
+                  title="Color Scheme Generator"
+                  description="An app that lets users generate, modify, and save color palettes."
+                  techStack={['React', 'Firebase Realtime Database', 'Firebase Authentication', 'Chakra UI']}
                   demoLink="#"
                   githubLink="#"
               />
