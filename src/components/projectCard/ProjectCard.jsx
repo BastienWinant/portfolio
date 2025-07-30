025b2f74a2531462b808f4de3f7c13d9325247ec
BastienWinant <bastien.winant@gmail.com>
2025-07-30 22:56:07 +0200

    stylistic changes

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import AnimatedLink from "@/components/animatedLink/AnimatedLink.jsx";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 import "./ProjectCard.css"
 import { FaGithub } from "react-icons/fa6";
-import arrowImg from "@/assets/img/20-arrow-right.svg"
+import arrowImg from "@/assets/img/12-arrow-right.svg"
 
 export default function ProjectCard({imgUrl, title, description, techStack, githubLink, demoLink}) {
 	return (
@@ -13,7 +13,7 @@ export default function ProjectCard({imgUrl, title, description, techStack, gith
 				<div className="project__body">
 					<p className="project__description">{description}</p>
 					<ul className="project__techstack">
-						{techStack.map(tech => <li>{tech}</li>)}
+						{techStack.map(tech => <li key={nanoid()}>{tech}</li>)}
 					</ul>
 				</div>
 				<footer className="project__links">
@@ -22,7 +22,10 @@ export default function ProjectCard({imgUrl, title, description, techStack, gith
 						className="project__demo_link"
 						target="_blank"
 					>
-						visit site
+						<span className="project__demo_link__text">
+							Visit Site
+							<img src={arrowImg} alt="Right arrow image." />
+						</span>
 					</AnimatedLink>
 					<a
 						href={githubLink}
